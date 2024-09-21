dbdaea23eef394188d46cc3c6a56618b7bcba28e
AomaYple <2890599214@qq.com>
2024-09-21 15:44:08 +0800

    小优化

--- src/server/src/coroutine/Scheduler.cpp
+++ src/server/src/coroutine/Scheduler.cpp
@@ -6,6 +6,18 @@
 #include "../ring/Ring.hpp"
 
 #include <ranges>
+#include <sys/resource.h>
+
+auto Scheduler::getFileDescriptorLimit(const std::source_location sourceLocation) -> unsigned long {
+    rlimit limit{};
+    if (getrlimit(RLIMIT_NOFILE, &limit) == -1) {
+        throw Exception{
+            Log{Log::Level::fatal, std::error_code{errno, std::generic_category()}.message(), sourceLocation}
+        };
+    }
+
+    return limit.rlim_cur;
+}
 
 auto Scheduler::registerSignal(const std::source_location sourceLocation) -> void {
     struct sigaction signalAction {};
@@ -41,15 +53,16 @@ Scheduler::Scheduler(const int sharedFileDescriptor, const unsigned int cpuCode,
         return ring;
     }()},
     main{main} {
+    const unsigned long fileDescriptorLimit{getFileDescriptorLimit()};
+
     this->ring->registerSelfFileDescriptor();
     this->ring->registerCpu(cpuCode);
-    this->ring->registerSparseFileDescriptor(Ring::getFileDescriptorLimit());
+    this->ring->registerSparseFileDescriptor(fileDescriptorLimit);
 
     std::vector fileDescriptors{Logger::create("log.log"), Server::create("127.0.0.1", 9090), Timer::create()};
     if (this->main) fileDescriptors.emplace_back(DatabaseManager::create());
 
-    this->ring->allocateFileDescriptorRange(fileDescriptors.size(),
-                                            Ring::getFileDescriptorLimit() - fileDescriptors.size());
+    this->ring->allocateFileDescriptorRange(fileDescriptors.size(), fileDescriptorLimit - fileDescriptors.size());
     this->ring->updateFileDescriptors(0, fileDescriptors);
 }
 
