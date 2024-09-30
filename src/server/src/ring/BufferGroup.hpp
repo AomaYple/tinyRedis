1833958bf1c62a59991a20fb204e23f275dbef08
AomaYple <2890599214@qq.com>
2024-09-30 15:25:12 +0800

    BufferGroup.hpp的格式化

--- src/server/src/ring/BufferGroup.hpp
+++ src/server/src/ring/BufferGroup.hpp
@@ -5,11 +5,11 @@
 
 class BufferGroup {
 public:
-  explicit BufferGroup(unsigned int count);
+    explicit BufferGroup(unsigned int count);
 
-  [[nodiscard]] auto getBuffer(unsigned short index) -> std::span<std::byte>;
+    [[nodiscard]] auto getBuffer(unsigned short index) -> std::span<std::byte>;
 
 private:
-  std::vector<std::byte> group{std::bit_ceil(2 * 1024 * 1024 / std::thread::hardware_concurrency())};
-  long size;
+    std::vector<std::byte> group{std::bit_ceil(2 * 1024 * 1024 / std::thread::hardware_concurrency())};
+    long size;
 };
