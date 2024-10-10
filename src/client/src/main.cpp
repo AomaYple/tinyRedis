e1a303d1c97304bb0396740e42354ac8c133ece7
AomaYple <2890599214@qq.com>
2024-10-10 12:27:33 +0800

    为客户端的formatRequest添加constexpr

--- src/client/src/main.cpp
+++ src/client/src/main.cpp
@@ -1,13 +1,68 @@
 #include "../../common/command/Command.hpp"
-#include "../../common/log/Exception.hpp"
 #include "network/Connection.hpp"
 
-#include <csignal>
 #include <iostream>
 #include <print>
 #include <utility>
 
-auto formatRequest(std::string_view data, unsigned long &id) -> std::vector<std::byte>;
+[[nodiscard]] constexpr auto formatRequest(std::string_view data, unsigned long &databaseIndex) {
+    const unsigned long space{data.find(' ')};
+    const auto command{data.substr(0, space)};
+    auto statement{data.substr(space + 1)};
+
+    Command commandType{};
+    if (command == "SELECT") {
+        commandType = Command::select;
+        databaseIndex = std::stoul(std::string{statement});
+        statement = {};
+    } else if (command == "DEL") commandType = Command::del;
+    else if (command == "EXISTS") commandType = Command::exists;
+    else if (command == "MOVE") commandType = Command::move;
+    else if (command == "RENAME") commandType = Command::rename;
+    else if (command == "RENAMENX") commandType = Command::renameNx;
+    else if (command == "TYPE") commandType = Command::type;
+    else if (command == "SET") commandType = Command::set;
+    else if (command == "GET") commandType = Command::get;
+    else if (command == "GETRANGE") commandType = Command::getRange;
+    else if (command == "GETBIT") commandType = Command::getBit;
+    else if (command == "SETBIT") commandType = Command::setBit;
+    else if (command == "MGET") commandType = Command::mGet;
+    else if (command == "SETNX") commandType = Command::setNx;
+    else if (command == "SETRANGE") commandType = Command::setRange;
+    else if (command == "STRLEN") commandType = Command::strlen;
+    else if (command == "MSET") commandType = Command::mSet;
+    else if (command == "MSETNX") commandType = Command::mSetNx;
+    else if (command == "INCR") commandType = Command::incr;
+    else if (command == "INCRBY") commandType = Command::incrBy;
+    else if (command == "DECR") commandType = Command::decr;
+    else if (command == "DECRBY") commandType = Command::decrBy;
+    else if (command == "APPEND") commandType = Command::append;
+    else if (command == "HDEL") commandType = Command::hDel;
+    else if (command == "HEXISTS") commandType = Command::hExists;
+    else if (command == "HGET") commandType = Command::hGet;
+    else if (command == "HGETALL") commandType = Command::hGetAll;
+    else if (command == "HINCRBY") commandType = Command::hIncrBy;
+    else if (command == "HKEYS") commandType = Command::hKeys;
+    else if (command == "HLEN") commandType = Command::hLen;
+    else if (command == "HSET") commandType = Command::hSet;
+    else if (command == "HVALS") commandType = Command::hVals;
+    else if (command == "LINDEX") commandType = Command::lIndex;
+    else if (command == "LLEN") commandType = Command::lLen;
+    else if (command == "LPOP") commandType = Command::lPop;
+    else if (command == "LPUSH") commandType = Command::lPush;
+    else if (command == "LPUSHX") commandType = Command::lPushX;
+
+    std::vector buffer{std::byte{std::to_underlying(commandType)}};
+
+    buffer.resize(buffer.size() + sizeof(databaseIndex));
+    *reinterpret_cast<std::remove_reference_t<decltype(databaseIndex)> *>(buffer.data() + buffer.size() -
+                                                                          sizeof(databaseIndex)) = databaseIndex;
+
+    const auto bytes{std::as_bytes(std::span{statement})};
+    buffer.insert(buffer.cend(), bytes.cbegin(), bytes.cend());
+
+    return buffer;
+}
 
 auto main() -> int {
     constexpr std::string_view host{"127.0.0.1"};
@@ -69,62 +124,3 @@ auto main() -> int {
 
     return 0;
 }
-
-auto formatRequest(std::string_view data, unsigned long &id) -> std::vector<std::byte> {
-    const unsigned long space{data.find(' ')};
-    const auto command{data.substr(0, space)};
-    auto statement{data.substr(space + 1)};
-
-    Command commandType{};
-    if (command == "SELECT") {
-        commandType = Command::select;
-        id = std::stoul(std::string{statement});
-        statement = {};
-    } else if (command == "DEL") commandType = Command::del;
-    else if (command == "EXISTS") commandType = Command::exists;
-    else if (command == "MOVE") commandType = Command::move;
-    else if (command == "RENAME") commandType = Command::rename;
-    else if (command == "RENAMENX") commandType = Command::renameNx;
-    else if (command == "TYPE") commandType = Command::type;
-    else if (command == "SET") commandType = Command::set;
-    else if (command == "GET") commandType = Command::get;
-    else if (command == "GETRANGE") commandType = Command::getRange;
-    else if (command == "GETBIT") commandType = Command::getBit;
-    else if (command == "SETBIT") commandType = Command::setBit;
-    else if (command == "MGET") commandType = Command::mGet;
-    else if (command == "SETNX") commandType = Command::setNx;
-    else if (command == "SETRANGE") commandType = Command::setRange;
-    else if (command == "STRLEN") commandType = Command::strlen;
-    else if (command == "MSET") commandType = Command::mSet;
-    else if (command == "MSETNX") commandType = Command::mSetNx;
-    else if (command == "INCR") commandType = Command::incr;
-    else if (command == "INCRBY") commandType = Command::incrBy;
-    else if (command == "DECR") commandType = Command::decr;
-    else if (command == "DECRBY") commandType = Command::decrBy;
-    else if (command == "APPEND") commandType = Command::append;
-    else if (command == "HDEL") commandType = Command::hDel;
-    else if (command == "HEXISTS") commandType = Command::hExists;
-    else if (command == "HGET") commandType = Command::hGet;
-    else if (command == "HGETALL") commandType = Command::hGetAll;
-    else if (command == "HINCRBY") commandType = Command::hIncrBy;
-    else if (command == "HKEYS") commandType = Command::hKeys;
-    else if (command == "HLEN") commandType = Command::hLen;
-    else if (command == "HSET") commandType = Command::hSet;
-    else if (command == "HVALS") commandType = Command::hVals;
-    else if (command == "LINDEX") commandType = Command::lIndex;
-    else if (command == "LLEN") commandType = Command::lLen;
-    else if (command == "LPOP") commandType = Command::lPop;
-    else if (command == "LPUSH") commandType = Command::lPush;
-    else if (command == "LPUSHX") commandType = Command::lPushX;
-
-    std::vector buffer{std::byte{std::to_underlying(commandType)}};
-
-    buffer.resize(buffer.size() + sizeof(id));
-    *reinterpret_cast<std::remove_reference_t<decltype(id)> *>(buffer.data() + buffer.size() - sizeof(id)) = id;
-
-    const auto bytes{std::as_bytes(std::span{statement})};
-    buffer.insert(buffer.cend(), bytes.cbegin(), bytes.cend());
-
-    return buffer;
-}
-
